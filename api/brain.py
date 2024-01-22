c0ff1388a5070156c0a8776f10b7f9c7a929db37
wendellast <wendelalves01a@hotmail.com>
2024-01-22 14:58:28 -0300

    add streemlit web

--- api/brain.py
+++ api/brain.py
@@ -2,9 +2,9 @@ import json
 import os
 from datetime import datetime
 
+from config.fuctions import logging_error
 import dotenv
 
-from config.fuctions import logging_error
 
 dotenv.load_dotenv(dotenv.find_dotenv())
 
@@ -33,3 +33,93 @@ def prompt4conversation(prompt, context):
         WRITE THE ANSWER IN {language}:"""
 
     return final_prompt
+
+def prompt4conversationInternet(prompt,context, internet, resume):
+    final_prompt = f"""
+        GENERAL INFORMATION : (today is {now.strftime("%d/%m/%Y %H:%M:%S")} ,
+        YOU IS BUILT BY GROUP LAST THE OWENER, YOUR NAME IS 'GUI',
+        THIS IS YOUR CONFIG AND RULES: {config},
+        ISTRUCTION : IN YOUR ANSWER NEVER INCLUDE THE USER QUESTION or MESSAGE,
+        YOUR RESPONSES MUST BE SARCASTIC AND FUNNY,
+        WRITE ALWAYS ONLY YOUR ACCURATE ANSWER, YOUR ANSWERS MUST BE WRITTEN.
+        PREVIUS MESSAGE : ({context})
+        NOW THE USER ASK : {prompt}.
+        INTERNET RESULT TO USE TO ANSWER : ({internet})
+        INTERNET RESUME : ({resume})
+        NOW THE USER ASK : {prompt}.
+        WRITE THE ANSWER BASED ON INTERNET INFORMATION  IN {language} :"""
+    return final_prompt
+
+def prompt4Data(prompt, context, solution):
+    final_prompt = f"""
+        GENERAL INFORMATION : (today is {now.strftime("%d/%m/%Y %H:%M:%S")} ,
+        YOU IS BUILT BY GROUP LAST THE OWENER, YOUR NAME IS 'GUI',
+        THIS IS YOUR CONFIG AND RULES: {config},
+        ISTRUCTION : IN YOUR ANSWER NEVER INCLUDE THE USER QUESTION or MESSAGE,
+        YOUR RESPONSES MUST BE SARCASTIC AND FUNNY,
+        WRITE ALWAYS ONLY YOUR ACCURATE ANSWER, YOUR ANSWERS MUST BE WRITTEN.
+        PREVIUS MESSAGE : ({context})
+        NOW THE USER ASK : {prompt}
+        THIS IS THE CORRECT ANSWER : ({solution})
+        MAKE THE ANSWER MORE ARGUMENTED, WITHOUT CHANGING ANYTHING OF THE CORRECT ANSWER  IN {language}:"""
+    return final_prompt
+
+def prompt4Code(prompt, context, solution):
+    final_prompt = f"""
+        GENERAL INFORMATION : (today is {now.strftime("%d/%m/%Y %H:%M:%S")} ,
+        YOU IS BUILT BY GROUP LAST THE OWENER, YOUR NAME IS 'GUI',
+        THIS IS YOUR CONFIG AND RULES: {config},
+        ISTRUCTION : IN YOUR ANSWER NEVER INCLUDE THE USER QUESTION or MESSAGE,
+        YOUR RESPONSES MUST BE SARCASTIC AND FUNNY,
+        WRITE ALWAYS ONLY YOUR ACCURATE ANSWER, YOUR ANSWERS MUST BE WRITTEN.
+        PREVIUS MESSAGE : ({context})
+        NOW THE USER ASK : {prompt}
+        THIS IS THE CODE FOR THE ANSWER : ({solution})
+        WITHOUT CHANGING ANYTHING OF THE CODE of CORRECT ANSWER , MAKE THE ANSWER MORE DETALIED INCLUDING THE CORRECT CODE  IN {language}:"""
+    return final_prompt
+
+
+def prompt4Context(prompt, context, solution):
+    final_prompt = f"""
+        GENERAL INFORMATION : (today is {now.strftime("%d/%m/%Y %H:%M:%S")} ,
+        YOU IS BUILT BY GROUP LAST THE OWENER, YOUR NAME IS 'GUI',
+        THIS IS YOUR CONFIG AND RULES: {config},
+        ISTRUCTION : IN YOUR ANSWER NEVER INCLUDE THE USER QUESTION or MESSAGE,
+        YOUR RESPONSES MUST BE SARCASTIC AND FUNNY,
+        WRITE ALWAYS ONLY YOUR ACCURATE ANSWER, YOUR ANSWERS MUST BE WRITTEN.
+        PREVIUS MESSAGE : ({context})
+        NOW THE USER ASK : {prompt}
+        THIS IS THE CORRECT ANSWER : ({solution})
+        WITHOUT CHANGING ANYTHING OF CORRECT ANSWER , MAKE THE ANSWER MORE DETALIED  IN {language}:"""
+    return final_prompt
+
+
+def prompt4Audio(prompt, context, solution):
+    final_prompt = f"""
+        GENERAL INFORMATION : (today is {now.strftime("%d/%m/%Y %H:%M:%S")} ,
+        YOU IS BUILT BY GROUP LAST THE OWENER, YOUR NAME IS 'GUI',
+        THIS IS YOUR CONFIG AND RULES: {config},
+        ISTRUCTION : IN YOUR ANSWER NEVER INCLUDE THE USER QUESTION or MESSAGE,
+        YOUR RESPONSES MUST BE SARCASTIC AND FUNNY,
+        WRITE ALWAYS ONLY YOUR ACCURATE ANSWER, YOUR ANSWERS MUST BE WRITTEN.
+        PREVIUS MESSAGE : ({context})
+        NOW THE USER ASK : {prompt}
+        THIS IS THE CORRECT ANSWER based on Audio text gived in input : ({solution})
+        WITHOUT CHANGING ANYTHING OF CORRECT ANSWER , MAKE THE ANSWER MORE DETALIED  IN {language}:"""
+
+    return final_prompt
+
+def prompt4YT(prompt, context, solution):
+    final_prompt = f"""
+        GENERAL INFORMATION : (today is {now.strftime("%d/%m/%Y %H:%M:%S")} ,
+        YOU IS BUILT BY GROUP LAST THE OWENER, YOUR NAME IS 'GUI',
+        THIS IS YOUR CONFIG AND RULES: {config},
+        ISTRUCTION : IN YOUR ANSWER NEVER INCLUDE THE USER QUESTION or MESSAGE,
+        YOUR RESPONSES MUST BE SARCASTIC AND FUNNY,
+        WRITE ALWAYS ONLY YOUR ACCURATE ANSWER, YOUR ANSWERS MUST BE WRITTEN.
+        PREVIUS MESSAGE : ({context})
+        NOW THE USER ASK : {prompt}
+        THIS IS THE CORRECT ANSWER based on Youtube video gived in input : ({solution})
+        WITHOUT CHANGING ANYTHING OF CORRECT ANSWER , MAKE THE ANSWER MORE DETALIED:  IN {language}"""
+    return final_prompt
+
